cb8ef52a6a987ec703b719efa3e4b323bd7480bf
ASPLAP3270 <dhanushbaskaran2000@gmail.com>
2022-12-19 18:05:46 +0530

    List and Keys

--- src/App.js
+++ src/App.js
@@ -2,21 +2,94 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const people = [
+  {id:1, name:"Dhanush"},
+  {id:2, name:"Hamza"},
+  {id:3, name:"Santhosh"},
+  {id:4, name:"Amar"},
+  {id:5, name:"Srini"},
+  {id:6, name:"mukesh"}
+  
+]
+
+const Developer = [{
+  name:'Dhanush',
+  skills:[
+    {name:'python'},
+    {name:'Java'},
+    {name:'Js'}
+  ]
+},
+{
+  name:'Hamza',
+  skills:[
+    {name:'Java'},
+  
+  ]
+},
+{
+  name:'Santhosh',
+  skills:[
+    {name:'python'},
+    {name:'Java'},
+    {name:'Js'},
+    {name:'React'}
+  ]
+}
+]
+
+const numbers = [1,2,3,4]
+const double = numbers.map(num=>
+  num*2
+)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <h1>React Js List and Keys</h1>
+
+    <ul>
+      {
+      people.map(data =>(
+        <li key = {data.id}>{data.name}</li>
+      ))
+      
+      }
+
+    </ul>
+    <div>
+      {Developer.map((Dev,index)=>(
+        <div key = {index}>
+          <h1>{Dev.name} Skills</h1>
+          <div>
+            {Dev.skills.map((skill,index)=>(
+              <li key={index}>{skill.name}</li>
+            ))}
+          </div>
+        </div>
+        ))}
+
+        <div>
+          
+        </div>
+     </div>
+     <div>
+     <ul>
+      <h1>Numbers</h1>
+      {numbers.map(num =>(
+            <li>{num}</li>
+          ))}</ul>
+      
+     </div>
+     <div>
+      <h1>Number Doubled</h1>
+      {double.map(num=>(
+        <li>{num}</li>
+      ))}
+      
+     </div>
+          
       </header>
     </div>
   );
